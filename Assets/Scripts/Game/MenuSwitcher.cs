ff51f4445f5a6c633285964b564d6f3d9ecaa478
CharmedGoose <charmedgoose1@gmail.com>
2025-01-01 18:36:01 -0500

    make esc exit settings to pause

--- Assets/Scripts/Game/MenuSwitcher.cs
+++ Assets/Scripts/Game/MenuSwitcher.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class SettingsMenuSwitcher : MonoBehaviour
 {
     GameObject gameUI;
+    GameObject menuUI;
+    GameObject pauseUI;
     GameObject settingsUI;
 
     InputAction menu;
@@ -11,9 +13,15 @@ public class SettingsMenuSwitcher : MonoBehaviour
     void Awake()
     {
         gameUI = transform.GetChild(0).gameObject;
-        settingsUI = transform.GetChild(1).gameObject;
+        menuUI = transform.GetChild(1).gameObject;
+
+        pauseUI = menuUI.transform.GetChild(1).gameObject;
+        settingsUI = menuUI.transform.GetChild(2).gameObject;
         
         gameUI.SetActive(true);
+        menuUI.SetActive(false);
+
+        pauseUI.SetActive(true);
         settingsUI.SetActive(false);
 
         menu = InputSystem.actions.FindAction("Menu");
@@ -31,9 +39,17 @@ public class SettingsMenuSwitcher : MonoBehaviour
 
     void ToggleUI()
     {
+        if (settingsUI.activeSelf)
+        {
+            settingsUI.SetActive(false);
+            pauseUI.SetActive(true);
+            return;
+        }
+
+        menuUI.SetActive(!menuUI.activeSelf);
         gameUI.SetActive(!gameUI.activeSelf);
-        settingsUI.SetActive(!settingsUI.activeSelf);
-        Cursor.lockState = settingsUI.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
-        Cursor.visible = settingsUI.activeSelf;
+
+        Cursor.lockState = menuUI.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = menuUI.activeSelf;
     }
 }
